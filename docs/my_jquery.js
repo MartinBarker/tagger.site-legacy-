7899f185e022c57095dd80a30e5f1ce8b95414ff
MartinBarker <martinbarker99@gmail.com>
2018-02-03 00:37:13 -0800

    js script import fix

---
+++ docs/my_jquery.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+  $('a').click(function(event) {
+    event.preventDefault();
+    $(this).hide("slow");
+  });
+});
--- docs/script.js
+++ docs/script.js
@@ -1,4 +1,12 @@
+
+
 var objectUrl;
+
+//<script
+//src="https://code.jquery.com/jquery-3.3.1.min.js"
+//integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
+//crossorigin="anonymous">
+//</script>
 
 $("#audio").on("canplaythrough", function(e){
     console.log("calculating duration");
